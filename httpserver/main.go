5b35ff7a2987fdcf6bebead839f07074d9cf3f6f
liuchongyu <liuchongyu@139.com>
2021-09-27 23:45:29 +0800

    课后练习

--- httpserver/main.go
+++ httpserver/main.go
@@ -1,20 +1,61 @@
 package main
+
 import (
-  "io"
+	"io"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-  mux := http.NewServeMux()
-	mux.HandleFunc("/healthz", healthz)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/healthz", requestHandle)
+	mux.HandleFunc("/header", requestHandle)
+	mux.HandleFunc("/env", requestHandle)
+	// 设置环境变量
+	setEnv()
 	err := http.ListenAndServe(":80", mux)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
+func setEnv() {
+	os.Setenv("VERSION", "1.0.0")
+}
+
+// 请求处理
+func requestHandle(w http.ResponseWriter, r *http.Request) {
+	switch r.RequestURI {
+	case "/healthz":
+		healthCheck(w, r)
+	case "/header":
+		injectResquestHeaders(w, r)
+	case "/env":
+		getEnv(w, r)
+	default:
+		w.WriteHeader(http.StatusNotFound)
+	}
+}
+
+// 获取环境变量
+func getEnv(w http.ResponseWriter, r *http.Request) {
+	version := os.Getenv("VERSION")
+	w.Header().Set("X-VERSION", version)
+}
+
+// 处理请求头
+func injectResquestHeaders(w http.ResponseWriter, r *http.Request) {
+	for name, values := range r.Header {
+		for _, value := range values {
+			w.Header().Set(name, value)
+		}
+	}
+	w.WriteHeader(http.StatusOK)
+}
+
 // 健康检查
-func healthz(w http.ResponseWriter, r *http.Request) {
+func healthCheck(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "200")
+	w.WriteHeader(http.StatusOK)
 }
