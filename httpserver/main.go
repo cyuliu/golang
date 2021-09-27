508f71cb1035a3f12d050a19cdfbe98d18cb9be6
Bamboo <liuchongyu007@163.com>
2021-09-27 23:10:54 +0800

    Create main.go

--- httpserver/main.go
+++ httpserver/main.go
@@ -1,1 +1,20 @@
+package main
+import (
+  "io"
+	"log"
+	"net/http"
+)
 
+func main() {
+  mux := http.NewServeMux()
+	mux.HandleFunc("/healthz", healthz)
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// 健康检查
+func healthz(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "200")
+}
