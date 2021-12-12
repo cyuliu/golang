c20aee9b8c1f8c582e361b4c9f650f3b4c866a49
liuchongyu <liuchongyu@139.com>
2021-12-13 01:40:35 +0800

    支持metrics

--- httpserver/main.go
+++ httpserver/main.go
@@ -1,4 +1,5 @@
 package main
+
 import (
 	"context"
 	"flag"
@@ -15,7 +16,8 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/cyuliu/golang/httpserver/metrics"
 )
-func main()  {
+
+func main() {
 	flag.Set("v", "4")
 	glog.V(2).Info("Starting http server...")
 	metrics.Register()
@@ -28,7 +30,18 @@ func main()  {
 func delayHandler(writer http.ResponseWriter, request *http.Request) {
 	glog.V(4).Info("Entering delay handler")
 	timer := metrics.NewTimer()
+	defer timer.ObserveTotal()
 
+	// 随机等待
+	delay := randInt(10, 2000)
+	time.Sleep(time.Duration(delay) * time.Millisecond)
+	glog.V(4).Infof("Respond in %d ms", delay)
+	glog.V(4).Info("Exiting delay handler")
+}
+
+func randInt(min int, max int) int {
+	rand.Seed(time.Now().UTC().UnixNano())
+	return min + rand.Intn(max-min)
 }
 
 func healthz(writer http.ResponseWriter, request *http.Request) {
